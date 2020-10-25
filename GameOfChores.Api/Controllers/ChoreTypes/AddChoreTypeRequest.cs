428ed0d3543d6d27d4e1835f00271b99fe033ab8
Dimitri KOCH <dkoch@sqli.com>
2020-10-25 21:31:35 +0100

    feat(chore-type): add Required attribute for chore type creation validation

--- GameOfChores.Api/Controllers/ChoreTypes/AddChoreTypeRequest.cs
+++ GameOfChores.Api/Controllers/ChoreTypes/AddChoreTypeRequest.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameOfChores.Api.Controllers.ChoreTypes
 {
     public class AddChoreTypeRequest
     {
+        [Required]
         public string Label { get; set; } = null!;
     }
 }
