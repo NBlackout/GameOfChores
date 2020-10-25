8cd8c1cc86f1ee214d00ea9f92d3913acbbb96f7
Dimitri KOCH <dkoch@sqli.com>
2020-10-25 21:52:59 +0100

    test(context): add dispose tests

---
+++ GameOfChores.Data.UnitTests/DbContextRepositoryTests/DisposeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+using GameOfChores.Data.Repositories;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Xunit;
+
+namespace GameOfChores.Data.UnitTests.DbContextRepositoryTests
+{
+    public class DisposeTests : InMemoryRepositoryTests<FakeDbRepository>
+    {
+        [Fact]
+        public void EnsureDisposed()
+        {
+            Repository.Dispose();
+
+            Func<DatabaseFacade> getContextDatabase = () => Repository.Context.Database;
+            getContextDatabase.Should().ThrowExactly<ObjectDisposedException>();
+        }
+    }
+
+    public class FakeDbRepository : DbContextRepository
+    {
+        public FakeDbRepository(GameOfChoresContext context)
+            : base(context)
+        {
+        }
+    }
+}
